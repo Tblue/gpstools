b984de58f8327cfc205bb8ecb8610788c3305877
Tilman Blumenbach <tilman+git@ax86.net>
2016-01-15 21:32:19 +0100

    Reformat code.

--- gpxannotate.py
+++ gpxannotate.py
@@ -37,75 +37,70 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-### IMPORTS ###
-import dateutil.parser
 import os
 import sys
 import tempfile
 
-from gps import EarthDistance
+import dateutil.parser
 from lxml import etree
 
+from gps import EarthDistance
 
-### HELPER FUNCTIONS ###
-def formatDistance( dist ):
+
+def format_distance(dist):
     if dist >= 1000:
-        return '%.2f km' % ( dist / 1000 )
+        return '%.2f km' % (dist / 1000)
 
     return '%.2f m' % dist
 
 
-###################
-### MAIN SCRIPT ###
-###################
-
-if len( sys.argv ) < 2:
+if len(sys.argv) < 2:
     print >> sys.stderr, 'Usage:'
     print >> sys.stderr, ' %s gpx-file [new-track-name...]' % sys.argv[0]
-    sys.exit( 1 )
+    sys.exit(1)
 
 # Try to load and parse the GPX file:
 try:
-    gpxTree = etree.parse( sys.argv[1] )
+    gpxTree = etree.parse(sys.argv[1])
 except IOError as e:
     print >> sys.stderr, "E: Could not open file `%s': %s" \
-            % (e.filename, e.strerror)
-    sys.exit( 2 )
+                         % (e.filename, e.strerror)
+    sys.exit(2)
 except etree.ParseError as e:
     print >> sys.stderr, "E: Could not parse file `%s': %s" \
-            % (sys.argv[1], e)
-    sys.exit( 3 )
+                         % (sys.argv[1], e)
+    sys.exit(3)
 
 # Great, file parsed! Determine the GPX version:
-gpxVer = gpxTree.getroot().get( 'version' )
-if not gpxVer or not gpxTree.getroot().tag.endswith( '}gpx' ):
+gpxVer = gpxTree.getroot().get('version')
+if not gpxVer or not gpxTree.getroot().tag.endswith('}gpx'):
     print >> sys.stderr, "E: File `%s' does not appear to be a valid GPX file!" \
-            % sys.argv[1]
-    sys.exit( 4 )
+                         % sys.argv[1]
+    sys.exit(4)
 elif gpxVer not in ["1.0", "1.1"]:
     print >> sys.stderr, "E: File `%s' has unsupported GPX version %s (need 1.0 or 1.1)." \
-            % (sys.argv[1], gpxVer)
-    sys.exit( 4 )
+                         % (sys.argv[1], gpxVer)
+    sys.exit(4)
 
 # Fix the namespace URI to reflect the GPX version we found.
 # TODO: Can this be done in a better way?
 gpxNamespaceURI = 'http://www.topografix.com/GPX/' + gpxVer.replace(".", "/")
-gpxNamespace    = '{' + gpxNamespaceURI + '}'
+gpxNamespace = '{' + gpxNamespaceURI + '}'
 
 # Find all <trk> elements:
-tracks = gpxTree.findall( gpxNamespace + 'trk' )
-if len( tracks ) == 0:
+tracks = gpxTree.findall(gpxNamespace + 'trk')
+if len(tracks) == 0:
     print >> sys.stderr, "E: No <trk> elements found in file `%s'!" \
-            % sys.argv[1]
-    sys.exit( 5 )
+                         % sys.argv[1]
+    sys.exit(5)
 
 # Now, try to process each <trk> element:
-for i in range( len( tracks ) ):
-    track    = tracks[i]
-    currname = track.findtext( gpxNamespace + 'name', '#' + str( i ) )
+for i in range(len(tracks)):
+    track = tracks[i]
+    currname = track.findtext(gpxNamespace + 'name', '#' + str(i))
 
-    points = track.findall( gpxNamespace + 'trkseg/' + gpxNamespace + 'trkpt' )
-    if len( points ) == 0:
+    points = track.findall(gpxNamespace + 'trkseg/' + gpxNamespace + 'trkpt')
+    if len(points) == 0:
         # According to the spec, this is not an error.
         print >> sys.stderr, 'W: Track', currname, 'has no segments and/or points! Skipping.'
         continue
@@ -114,37 +109,37 @@ for i in range( len( tracks ) ):
     print '--------------'
 
     # Make sure there is a description element for this track.
-    descElm = track.find( gpxNamespace + 'desc' )
+    descElm = track.find(gpxNamespace + 'desc')
     if descElm is None:
-        descElm = etree.SubElement( track, gpxNamespace + 'desc' )
+        descElm = etree.SubElement(track, gpxNamespace + 'desc')
         descElm.text = ''
     else:
         descElm.text += '\n'
 
     # Calculate the distance of this track.
     distance = 0
-    for j in range( 1, len( points ) ):
-        aKeys = points[j-1].keys()
+    for j in range(1, len(points)):
+        aKeys = points[j - 1].keys()
         bKeys = points[j].keys()
 
         if 'lat' not in aKeys or 'lon' not in aKeys or \
                 'lat' not in bKeys or 'lon' not in bKeys:
             print >> sys.stderr, 'E: While processing track %s:' % currname, \
-                    "<trkpt> is missing `lon' and/or `lat' attributes."
-            sys.exit( 4 )
+                "<trkpt> is missing `lon' and/or `lat' attributes."
+            sys.exit(4)
 
         try:
-            pointA = (float( points[j-1].get( 'lat' ) ), float( points[j-1].get( 'lon' ) ))
-            pointB = (float( points[j].get( 'lat' ) ), float( points[j].get( 'lon' ) ))
+            pointA = (float(points[j - 1].get('lat')), float(points[j - 1].get('lon')))
+            pointB = (float(points[j].get('lat')), float(points[j].get('lon')))
         except ValueError as e:
             print >> sys.stderr, 'E: While processing track %s:' % currname, \
-                    "<trkpt> has invalid value for `lon' and/or `lat' attributes:", e
-            sys.exit( 4 )
+                "<trkpt> has invalid value for `lon' and/or `lat' attributes:", e
+            sys.exit(4)
 
-        distance += EarthDistance( pointA, pointB )
+        distance += EarthDistance(pointA, pointB)
 
-    descElm.text += 'Distance:\t' + formatDistance( distance )
-    print 'Distance:\t%s' % formatDistance( distance )
+    descElm.text += 'Distance:\t' + format_distance(distance)
+    print 'Distance:\t%s' % format_distance(distance)
 
     # If possible, calculate the track's duration.
     if len(points) > 1:
@@ -157,67 +152,67 @@ for i in range( len( tracks ) ):
             duration = endTime - startTime
 
             durStr = 'Duration:\t%s\nStart time:\t%s\nEnd time:\t%s' % (
-                    duration,
-                    startTime.strftime('%Y-%m-%d %H:%M %Z'),
-                    endTime.strftime('%Y-%m-%d %H:%M %Z')
-                )
+                duration,
+                startTime.strftime('%Y-%m-%d %H:%M %Z'),
+                endTime.strftime('%Y-%m-%d %H:%M %Z')
+            )
             descElm.text += '\n' + durStr
             print durStr
 
-    if i + 2 < len( sys.argv ):
+    if i + 2 < len(sys.argv):
         # Rename track.
         print "Renaming to `%s'." % sys.argv[i + 2]
 
-        nameElm = track.find( gpxNamespace + 'name' )
+        nameElm = track.find(gpxNamespace + 'name')
         if nameElm is None:
-            nameElm = etree.SubElement( track, gpxNamespace + 'name' )
+            nameElm = etree.SubElement(track, gpxNamespace + 'name')
 
-        nameElm.text = sys.argv[i + 2].decode( 'utf-8' )
+        nameElm.text = sys.argv[i + 2].decode('utf-8')
 
     print
 
 # Add some credit. :)
-myName = os.path.basename( sys.argv[0] ).decode( 'utf-8' )
-gpxCreator = gpxTree.getroot().get( 'creator' )
+myName = os.path.basename(sys.argv[0]).decode('utf-8')
+gpxCreator = gpxTree.getroot().get('creator')
 if not gpxCreator:
     gpxCreator = myName
 else:
     gpxCreator += ' (processed by %s)' % myName
 
-gpxTree.getroot().set( 'creator', gpxCreator )
+gpxTree.getroot().set('creator', gpxCreator)
 
 # First, write to a temporary file...
 tmpFile = None
 
 try:
     tmpFile = tempfile.NamedTemporaryFile(
-            dir=os.path.dirname( sys.argv[1] ),
-            prefix=os.path.basename( sys.argv[1] ) + '.',
+            dir=os.path.dirname(sys.argv[1]),
+            prefix=os.path.basename(sys.argv[1]) + '.',
             suffix='.new',
             delete=False
-        )
-    gpxTree.write( tmpFile, pretty_print=True )
+    )
+    gpxTree.write(tmpFile, pretty_print=True)
 except EnvironmentError as e:
     print >> sys.stderr, "E: Could not create/write to temporary file `%s': %s" \
-            % (e.filename, e.strerror)
+                         % (e.filename, e.strerror)
 
     if tmpFile is not None:
         tmpFile.close()
-        os.unlink( tmpFile.name )
+        os.unlink(tmpFile.name)
 
-    sys.exit( 6 )
+    sys.exit(6)
 
 # ...fsync...
 tmpFile.flush()
-os.fsync( tmpFile.fileno() )
+os.fsync(tmpFile.fileno())
 tmpFile.close()
 
 # ...then move the tempfile to the source file.
 try:
-    os.rename( tmpFile.name, sys.argv[1] )
+    os.rename(tmpFile.name, sys.argv[1])
 except OSError as e:
     print >> sys.stderr, "E: Could not move file `%s' to `%s': %s" \
-            % (tmpFile.name, sys.argv[1], e.strerror)
+                         % (tmpFile.name, sys.argv[1], e.strerror)
 
-    os.unlink( tmpFile.name )
-    sys.exit( 6 )
+    os.unlink(tmpFile.name)
+    sys.exit(6)
